bd5e02c1229195b1fa5dde52a32475b48a1dbffe
Kitty Belling <katbelling@gmail.com>
2023-12-01 11:23:01 -0800

    auto-commit from python : 20231201_112300

--- day1/day1.py
+++ day1/day1.py
@@ -8,15 +8,52 @@ line_outputs = []
 
 # FUNCTION
 def get_first_number(instr):
-    for i in instr:
-        if i.isdigit():
-            return i
+    #for i in instr:
+    #    if i.isdigit():
+    #        return i
+    for i in range(len(instr)):
+        if instr[i].isdigit():
+            return instr[i]
+        else:
+            l = check_for_spelled_letter(instr[i:])
+            if l != "":
+                return l
+
+def check_for_spelled_letter(instr):
+    if len(instr) >= 3:
+        #print(instr[0:3])
+        if instr[0:3] == "one":
+            return "1"
+        elif instr[0:3] == 'two':
+            return "2"
+        elif instr[0:3] == "six":
+            return "6"
+    if len(instr) >= 4:
+        if instr[0:4] == "four":
+            return "4"
+        elif instr[0:4] == "five":
+            return "5"
+        elif instr[0:4] == "nine":
+            return "9"
+    if len(instr) >= 5:
+        if instr[0:5] == "three":
+            return "3"
+        elif instr[0:5] == "seven":
+            return "7"
+        elif instr[0:5] == "eight":
+            return "8"
+    return ""
+    
 
 def get_last_number(instr):
     for i in range(len(instr)):
         j = len(instr) - i - 1
         if instr[j].isdigit():
             return instr[j]
+        else:
+            l = check_for_spelled_letter(instr[j:])
+            if l != "":
+                return l
 
 # MAIN
 iLines = chelp.get_input_text("input.txt")
@@ -30,7 +67,7 @@ for line in iLines:
     #print(o)
     line_outputs.append(o)
 
-#print(line_outputs)
+print(line_outputs)
 
 s = 0
 for o in line_outputs:
