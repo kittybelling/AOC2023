9c3b1b4d1913fedc2d02f588f1e150d720567b08
Kitty Belling <katbelling@gmail.com>
2023-12-03 01:49:18 -0800

    auto-commit from python : 20231203_014917

--- day3/day3.py
+++ day3/day3.py
@@ -10,6 +10,9 @@ chelp = aoc_helper()
 grid = []
 
 # FUNCTION
+
+# Part 1
+
 def create_grid(iLines):
     w = len(iLines[0])
     l = len(iLines)
@@ -116,6 +119,84 @@ def get_sum(numbers):
         s += int(n)
     return s
 
+# Part 2
+
+def create_grid_2(iLines):
+    w = len(iLines[0])
+    l = len(iLines)
+    global grid
+    for i in range(l):
+        s = []
+        for j in range(w):
+            x = iLines[i][j]
+            if x.isdigit():
+                s.append("d")
+            elif x == ".":
+                s.append(".")
+            elif x == "*":
+                s.append("x")
+            else:
+                s.append("o")
+        grid.append(s.copy())
+
+def parse_grid_2(iLines):
+    global grid
+    gear_ratio = 0
+    for i in range(len(iLines)):
+        for j in range(len(iLines[0])):
+            if grid[i][j] == "x":
+                lemons = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
+                c = 0
+                nums = []
+                for life in lemons:
+                    lemonade = check_coord(i,j,life[0],life[1],"d")
+                    if lemonade:
+                        c += 1
+                        n = get_digit(i+life[0],j+life[1],iLines)
+                        nums.append(n)
+                if c == 2:
+                    gear_ratio += (nums[0] * nums[1]) 
+    return gear_ratio
+
+def check_coord(i,j,m,n,c):
+    global grid
+    try:
+        if grid[i+m][j+n] == c:
+            return True
+        else:
+            return False 
+    except:
+        return False
+
+def get_digit(i,j,iLines):
+    global grid
+    g = iLines[i][j]
+    grid[i][j] = iLines[i][j]
+    done = False
+    y = j
+    while not done:
+        y-=1
+        try:
+            if grid[i][y] == "d":
+                g = iLines[i][y] + g 
+                grid[i][y] = iLines[i][y]
+            else:
+                done = True 
+        except:
+            done = True 
+    y = j 
+    done = False
+    while not done:
+        y += 1
+        try: 
+            if grid[i][y] == "d":
+                g = g + iLines[i][y]
+                grid[i][y] = iLines[i][y]
+            else:
+                done = True 
+        except:
+            done = True 
+    return int(g)
 
 
 # MAIN
@@ -123,16 +204,29 @@ iLines = chelp.get_input_text("input.txt")
 
 # Part 1
 
-create_grid(iLines)
+#create_grid(iLines)
+##for g in grid:
+##    print(g)
+
+#print("")
+
+#parse_grid(iLines)
 #for g in grid:
 #    print(g)
 
-print("")
+#s, numbers = calculate_grid()
+#print("Numbers:\t", numbers) 
+#print("Sum:\t\t", s)
 
-parse_grid(iLines)
-for g in grid:
-    print(g)
+# Part 2
 
-s, numbers = calculate_grid()
-print("Numbers:\t", numbers) 
-print("Sum:\t\t", s)
+create_grid_2(iLines)
+#for g in grid:
+#    print(g)
+#print("")
+
+gear_ratio = parse_grid_2(iLines) 
+#for g in grid:
+#    print(g)
+
+print("Gear ratio:\t",gear_ratio)
